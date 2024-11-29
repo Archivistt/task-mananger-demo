04d32b7e6c1fdddfc6dc0e6c34d93119fbec867a
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-11-29 13:00:23 +0000

    Created fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { db } from './firebase'
+import { collection, doc, getDocs, deleteDoc, updateDoc, getDoc } from "firebase/firestore";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  //Fetch the documents from the colletion
+  const fetchTasks = async ()=> {
+    const collectionRef = collection(db, 'tasks');
+    const querySnapshot = await getDocs(collectionRef);
+    const tasks = querySnapshot.docs.map((task) => ({
+      id: task.id,
+      ...task.data() //the rest of the data
+    }))
+    setTasks(tasks)
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, [])
 
   return (
     <>
-      
+      {
+        tasks.map((task) => (
+          <div> 
+            <div>
+              Task title: {task.title} 
+            </div>
+            <div>
+              Task body: {task.body} 
+            </div>
+            <div>
+              Task status: {task.status} 
+            </div>
+          </div>
+          
+        ))
+      }
     </>
   )
 }
